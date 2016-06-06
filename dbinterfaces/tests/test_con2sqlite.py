541ab1b03ac4aaddc352826549d67408fce1fd7d
stefanseltmann77 <s.seltmann06@web.de>
2016-06-06 14:50:07 +0200

    fixes

--- dbinterfaces/tests/test_con2sqlite.py
+++ dbinterfaces/tests/test_con2sqlite.py
@@ -28,7 +28,7 @@ class TestCon2SQLite(TestCase):
         result = self.db.query_value("SELECT 1")
         self.assertEqual(result, 1)
 
-    def test_query_value(self):
+    def test_query_row(self):
         result = self.db.query_row("SELECT 1, 2, 3")
         self.assertIsInstance(result, dict)
         self.assertEqual(result['1'], 1)
@@ -36,87 +36,7 @@ class TestCon2SQLite(TestCase):
     def test_insert_row(self):
         self.db.insert_row(table_name='testtable', params=(1, 2))
 
-    def test_silent_mode_1(self):
-        console = io.StringIO()
-        sys.stdout = console
-        self.db._output_streams = {self.db.OS_DATABASE}
-        self.db.query("SELECT 1")
-        self.assertEqual("", console.getvalue())
+    def test_query_schema_current(self):
+        schema = self.db.query_schema_current()
+        print(schema)
 
-    def test_silent_mode_2(self):
-        console = io.StringIO()
-        sys.stdout = console
-        self.db._output_streams = {self.db.OS_DATABASE}
-        self.db.echo_sql = True
-        self.db.query("SELECT 1")
-        self.assertEqual("", console.getvalue())
-
-    def test_silent_mode_3(self):
-        console = io.StringIO()
-        sys.stdout = console
-        self.db._output_streams = {self.db.OS_DATABASE, self.db.OS_STDOUT}
-        self.db.echo_sql = False
-        self.db.query("SELECT 1")
-        self.assertEqual("", console.getvalue())
-
-    def test_silent_mode_4(self):
-        console = io.StringIO()
-        sys.stdout = console
-        self.db._output_streams = {self.db.OS_DATABASE, self.db.OS_STDOUT}
-        self.db.echo_sql = True
-        self.db.query("SELECT 1", echo=False)
-        self.assertEqual("", console.getvalue())
-
-    def test_silent_mode_5(self):
-        console = io.StringIO()
-        sys.stdout = console
-        self.db._output_streams = {self.db.OS_DATABASE, self.db.OS_STDOUT}
-        self.db.echo_sql = False
-        self.db.query("SELECT 1", echo=False)
-        self.assertEqual("", console.getvalue())
-
-    def test_echo_mode_1(self):
-        console = io.StringIO()
-        sys.stdout = console
-        self.db._output_streams = {self.db.OS_DATABASE, self.db.OS_STDOUT}
-        self.db.echo_sql = False
-        self.db.query("SELECT 1", echo=True)
-        self.assertNotEqual("", console.getvalue())
-
-    def test_echo_mode_2(self):
-        console = io.StringIO()
-        sys.stdout = console
-        self.db._output_streams = {self.db.OS_STDOUT}
-        self.db.echo_sql = False
-        self.db.query("SELECT 1", echo=True)
-        self.assertNotEqual("", console.getvalue())
-
-    def test_echo_mode_3(self):
-        console = io.StringIO()
-        sys.stdout = console
-        self.db._output_streams = {self.db.OS_STDOUT}
-        self.db.echo_sql = True
-        self.db.query("SELECT 1")
-        self.assertNotEqual("", console.getvalue())
-
-    def test_echo_mode_4(self):
-        console = io.StringIO()
-        sys.stdout = console
-        self.db._output_streams = {self.db.OS_STDOUT}
-        self.db.query("SELECT 1")
-        self.assertNotEqual("", console.getvalue())
-
-    def test_echo_mode_5(self):
-        console = io.StringIO()
-        sys.stdout = console
-        self.db._output_streams = {self.db.OS_STDOUT}
-        self.db.echo_sql = True
-        self.db.query("SELECT 1", is_meta=False)
-        self.assertNotEqual("", console.getvalue())
-
-    def test_echo_mode_6(self):
-        console = io.StringIO()
-        sys.stdout = console
-        self.db._output_streams = {self.db.OS_STDOUT}
-        self.db.query("SELECT 1", is_meta=False)
-        self.assertNotEqual("", console.getvalue())
